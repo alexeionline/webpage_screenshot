2a661f04e9131ce038f8e542cb5db45091c8bcfd
alexeionline <alexeisevua@gmail.com>
2013-09-30 15:10:26 +0300

    app init

---
+++ app.js
@@ -0,0 +1,30 @@
+var   express           = require('express')
+    , http              = require('http')
+    , path              = require('path')
+    , fs                = require('fs')
+    , app               = express()
+	;
+
+app.configure(function () {
+
+	app.set('port', process.env.PORT || 8000);
+	app.set('views', __dirname + '/views');
+	app.set('view engine', 'jade');
+	app.use(express.favicon());
+	app.use(express.logger('dev'));
+	app.use(express.bodyParser());
+	app.use(express.methodOverride());
+	app.use(app.router);
+	app.use(express.static(path.join(__dirname, 'public')));
+
+});
+
+var server = http.createServer(app).listen(app.get('port'), function () {
+	console.log('server is running on ' + app.get('port'))
+});
+
+app.get('/',  function (req, res) {
+	res.render('index', {title: 'home page'});
+});
+
+

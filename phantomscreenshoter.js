40e60cc825061cb8196ddc21c41cd4e8d306a750
Eugene Glova <eugene.glova@gmail.com>
2013-10-01 16:30:59 +0300

    add custom sizes support via url params

--- phantomscreenshoter.js
+++ phantomscreenshoter.js
@@ -1,7 +1,14 @@
 var page        = require('webpage').create();
 var system      = require('system');
 var url         = system.args[1];
+var width       = system.args[2];
+var height      = system.args[3];
 var address     = 'http://' + url;
+
+if (width && height) {
+    page.viewportSize = { width: width, height: height };
+    page.clipRect = { top: 0, left: 0, width: width, height: height };
+}
 
 page.open(address, function () {
     page.render('public/images/' + url + '.png');
